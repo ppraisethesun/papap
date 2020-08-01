ca45681c176ab67e57b1f2ed3c4e046dd1081b28
Roman <ppraisethesun@gmail.com>
2020-08-01 23:24:02 +0300

    nimble

--- lib/nimble.ex
+++ lib/nimble.ex
@@ -3,12 +3,12 @@ defmodule Nimble do
 
   defmodule Base do
     def ignore_ws(combinator) do
-      ignore(combinator, choice([repeat(ascii_char([?\s])), empty()]))
+      ignore(combinator, choice([repeat(ascii_char([?\s])), empty()])) |> label("whitespace")
     end
   end
 
-  opening_bracket = string("(") |> Base.ignore_ws() |> label("opening bracket")
-  closing_bracket = string(")") |> Base.ignore_ws() |> label("closing bracket")
+  opening_bracket = string("(") |> byte_offset() |> Base.ignore_ws() |> label("opening bracket")
+  closing_bracket = string(")") |> byte_offset() |> Base.ignore_ws() |> label("closing bracket")
 
   add_sub = utf8_string([?+, ?-], 1) |> Base.ignore_ws() |> label("operator")
   mul_div = utf8_string([?*, ?/], 1) |> Base.ignore_ws() |> label("operator")
@@ -17,11 +17,15 @@ defmodule Nimble do
   func_closing_bracket = string("}") |> Base.ignore_ws() |> label("function closing bracket")
   func_args_separator = string(",") |> Base.ignore_ws() |> label("args separator")
 
+  any_operator = utf8_string([?+, ?-, ?*, ?/, ?<, ?>, ?=], min: 1) |> label("any operator")
+  any_closing_bracket = utf8_string([?), ?}], min: 1) |> label("any closing bracket")
+
   integer =
     integer(min: 1)
     |> label("integer")
     |> lookahead_not(string("."))
     |> map({__MODULE__, :wrap, [:integer]})
+    |> byte_offset()
     |> Base.ignore_ws()
 
   float =
@@ -29,6 +33,7 @@ defmodule Nimble do
     |> label("whole part")
     |> label(string("."), "decimal separator")
     |> label(integer(min: 1), "decimal part")
+    |> byte_offset()
     |> Base.ignore_ws()
 
   ident =
@@ -42,6 +47,7 @@ defmodule Nimble do
 
   variable =
     ident
+    |> byte_offset()
     |> Base.ignore_ws()
     |> lookahead_not(func_opening_bracket)
     |> map({__MODULE__, :wrap, [:variable]})
@@ -59,52 +65,132 @@ defmodule Nimble do
       |> parsec(:expression)
     )
     |> wrap()
+    |> label("function arguments")
+
+  function_name = ident |> label("function name")
 
   function_call =
-    ident
+    function_name
     |> ignore(func_opening_bracket)
-    |> concat(func_call_args)
+    |> concat(optional(func_call_args))
     |> ignore(func_closing_bracket)
     |> wrap()
+    |> label("function call")
     |> map({__MODULE__, :wrap, [:function_call]})
+    |> debug()
 
   expression_in_brackets =
-    ignore(opening_bracket)
+    opening_bracket
     |> parsec(:expression)
-    |> ignore(closing_bracket)
+    |> optional(closing_bracket)
+    |> post_traverse({:what_is_this, []})
+
+  def what_is_this(_rest, [_closing, expr, _opening], context, _line, _offset) do
+    IO.inspect(expr)
+    {[expr], context}
+  end
+
+  def what_is_this(_rest, [_args, {paren, position}] = args, _, _, _) do
+    {:error, "no match for #{paren} at #{position}"}
+  end
 
   factor =
     choice([
       number,
       variable,
-      expression_in_brackets,
-      function_call
+      function_call,
+      expression_in_brackets
     ])
+    |> Base.ignore_ws()
+    |> label("factor")
 
   defparsec(
     :term,
-    choice([
-      factor
-      |> concat(mul_div)
-      |> parsec(:term)
-      |> wrap()
-      |> map({__MODULE__, :wrap, [:operator]}),
-      factor
-    ])
+    factor
+    |> repeat(mul_div |> concat(factor))
+    |> wrap()
+    |> map({__MODULE__, :left_ass, []})
   )
+
+  expr_start =
+    empty()
+    |> Base.ignore_ws()
+    |> lookahead_not(choice([any_operator, any_closing_bracket]))
 
   defparsec(
     :expression,
-    choice([
-      parsec(:term)
-      |> concat(add_sub)
-      |> parsec(:expression)
-      |> wrap()
-      |> map({__MODULE__, :wrap, [:operator]}),
-      parsec(:term)
-    ])
-    |> label("expression")
+    expr_start
+    |> parsec(:term)
+    |> repeat(add_sub |> parsec(:term))
+    |> wrap()
+    |> map({__MODULE__, :left_ass, []})
   )
+
+  defparsec(:full, parsec(:expression) |> eos())
+
+  comparison_operator =
+    choice([
+      string(">"),
+      string("<"),
+      string("="),
+      string("<="),
+      string(">="),
+      string("<>")
+    ])
+    |> Base.ignore_ws()
+    |> label("comparison operator")
+
+  defparsec(
+    :comparison,
+    parsec(:expression)
+    |> concat(comparison_operator)
+    |> parsec(:expression)
+    |> lookahead_not(comparison_operator)
+    |> wrap()
+    |> map({__MODULE__, :wrap_comparison, []})
+  )
+
+  def wrap_comparison([a, op, b]) do
+    %{
+      data: [operator: op],
+      args: [a, b]
+    }
+  end
+
+  @doc """
+  wrap left associative operators
+
+  1 - 2 - 3 == (1 - 2) - 3 != 1 - (2 - 3)
+  """
+  def left_ass([a]), do: a
+
+  def left_ass([a, op, b | rest]) do
+    new_a = %{
+      data: [operator: op],
+      args: [a, b]
+    }
+
+    left_ass([new_a | rest])
+  end
+
+  def wrap([a, op, b], :term) do
+    %{
+      data: [operator: op],
+      args: List.flatten([a, b])
+    }
+  end
+
+  def wrap([%{data: _}] = term, :term) do
+    term
+  end
+
+  def wrap(term, :term) when is_list(term) do
+    __MODULE__.wrap(List.flatten(term), :term)
+  end
+
+  def wrap(term, :term) do
+    term |> IO.inspect()
+  end
 
   def wrap(term, :integer), do: %{data: [integer: term]}
   def wrap(term, :variable), do: %{data: [variable: term]}
@@ -119,7 +205,14 @@ defmodule Nimble do
   def wrap([func_name, args], :function_call) do
     %{
       data: [function: func_name],
-      args: args
+      args: List.flatten(args)
+    }
+  end
+
+  def wrap([func_name], :function_call) when is_binary(func_name) do
+    %{
+      data: [function: func_name],
+      args: []
     }
   end
 end
